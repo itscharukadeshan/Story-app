e7b447594b1be3513c0c37f36ec6064829ae443e
charuka deshan <itscharukadeshan@gmail.com>
2023-05-15 19:14:11 +0530

    add zk editor to body of the form

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ require("./config/passport")(passport);
 connectDB();
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -41,6 +45,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3001;
 
---
+++ routes/stories.js
@@ -0,0 +1,22 @@
+/** @format */
+
+const express = require("express");
+const router = express.Router();
+const Story = require("../models/Story");
+const { ensureAuth } = require("../middleware/auth");
+
+router.get("/add", ensureAuth, (req, res) => {
+  res.render("stories/add");
+});
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    await Story.create(req.body);
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.log(err);
+    res.render("errors/500");
+  }
+});
+
+module.exports = router;
