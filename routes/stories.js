cf7b21da382c5cd9a0009e58775f3b9a5ab0e3ba
charuka deshan <itscharukadeshan@gmail.com>
2023-05-17 12:20:23 +0530

    fixed typo

--- routes/stories.js
+++ routes/stories.js
@@ -18,14 +18,16 @@ router.post("/", ensureAuth, async (req, res) => {
     res.render("errors/500");
   }
 });
+
 router.get("/", ensureAuth, async (req, res) => {
   try {
     const stories = await Story.find({ status: "public" })
       .populate("user")
       .sort({ createdAt: "desc" })
       .lean();
-
-    res.render("stories/index", { stories });
+    res.render("stories/index", {
+      stories,
+    });
   } catch (err) {
     console.error(err);
     res.render("errors/500");
