f170cbc3b4e947a5da0d30ea47dfc1a6932b42bf
charuka deshan <itscharukadeshan@gmail.com>
2023-05-17 15:47:45 +0530

    add method override to use put req in form

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const methodOverride = require("method-override");
 const exphbs = require("express-handlebars");
 const connectDB = require("./config/db");
 const morgan = require("morgan");
@@ -19,6 +20,16 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
