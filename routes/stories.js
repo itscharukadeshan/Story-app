15be389c75a81e9ae40dbb2a61c28c534f4e2ecc
charuka deshan <itscharukadeshan@gmail.com>
2023-05-17 19:56:46 +0530

    add delete function

--- routes/stories.js
+++ routes/stories.js
@@ -57,16 +57,31 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
 });
 
 router.put("/:id", ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id).lean();
-  if (!story) {
-    res.render("/stories");
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
+  try {
+    let story = await Story.findById(req.params.id).lean();
+    if (!story) {
+      res.render("/stories");
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
 
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("errors/500");
+  }
+});
+
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    await Story.findOneAndRemove({ _id: req.params.id });
     res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
   }
 });
 module.exports = router;
